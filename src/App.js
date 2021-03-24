fc244a0819ac95b43536033f78914a7314bc4f51
Dave Inoc <michael828inoc@gmail.com>
2021-03-24 16:41:50 +0800

    react practice

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react"
+import './App.scss';
 
 function App() {
+
+    let user1 = {
+      name: "John Doe",
+      age: 51,
+      email: "johndoe@gmail.com"
+    }
+    let user2 = {
+      name: "cmiahe Tradio",
+      age: 11,
+      email: "cmiahetradio@gmail.com"
+    }
+    let user3 = {
+      name: "Break Valuenzela",
+      age: 51,
+      email: "breakvalenzuela@gmail.com"
+    }
+
+  const [user, setUser] = useState(user1)
+
+  const getuser1 = () => { 
+    setUser(user1)
+  }
+  const getuser2 = () => { 
+    setUser(user2)
+  }
+  const getuser3 = () => { 
+    setUser(user3)
+  }
+  console.log(user.name)
+  console.log(user.age)
+  console.log(user.email)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+        <h1>{user.name}</h1>
+        <p>{user.age}</p>
+        <p>{user.email}</p>
+        <button onClick={getuser1}>Get User 1</button>
+        <button onClick={getuser2}>Get User 2</button>
+        <button onClick={getuser3}>Get User 3</button>
+    </>
   );
 }
 
